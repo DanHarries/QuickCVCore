12090be7a9568b001b31edbf015bccee4cc2765e
Mr H <dan_d_h@outlook.com>
2019-02-09 13:44:19 +0000

    Preview page front end and ironpdf nuget in solution

---
+++ Controllers/ExportCVController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using QuickCV.Models;
+
+namespace QuickCV.Controllers
+{
+    public class ExportCVController : Controller
+    {
+        public IActionResult Preview(CVDetailsViewModel model)
+        {
+            return View(model);
+        }
+    }
+}
---
+++ Models/CVDetailsViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuickCV.Models
+{
+    public class CVDetailsViewModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public string Address2 { get; set; }
+        public string Country { get; set; }
+        public string County { get; set; }
+        public string Postcode { get; set; }
+
+    }
+}
